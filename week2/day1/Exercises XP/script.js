1dcb0bf77232f38182cba5386ee335458926b5d2
YechielNW <yechielallaround@gmail.com>
2021-04-11 13:16:52 +0300

    JSGuess

---
+++ week2/day1/Exercises XP/script.js
@@ -0,0 +1,33 @@
+// 1 Guess the correct outcome to the lines of code written below.
+
+// 5 + "34"                          "534"
+// 5 - "4"                           "1"
+// 10 % 5                            1
+// 5 % 10                            2
+// "Java" + "Script"             JavaScript
+// " " + " "                      null
+// " " + 0                         null
+// true + true                      2
+// true + false                     1
+// false + true                    1
+// false - true                     -1
+// 3 - 4                            -1
+// "Bob" - "bill"                   nan
+//
+// 2 After guessing the outcome check the results of the code below.
+
+// 3 Check them by copying line by line and running them in the console.
+
+// 5 + "34"          "534"
+// 5 - "4"             1
+// 10 % 5              0
+// 5 % 10               5
+// "Java" + "Script"      "JavaScript"
+// " " + " "              "  "
+// " " + 0                "0"
+// true + true             2
+// true + false            1
+// false + true            1
+// false - true            -1
+// 3 - 4                   -1
+// "Bob" - "bill"          nan
---
+++ week2/day1/Javascript intro/script.js
@@ -0,0 +1,60 @@
+var greating = 'hello';
+
+console.log(greating);
+
+// to check kind
+// console.log(typeof(greating)) // greeting is a string
+
+// console.log(greating.substring(1,4))
+
+// //strings primitive data types
+
+// let usersAge = 45;
+// console.log(typeof(usersAge));
+
+// let icecream;
+
+//Ex1
+let addressNumber = 6800;
+let addressStreet = "5th"
+let country = 'U.S.A.'
+
+let global_address = "I live at " + addressNumber +" "+ addressStreet + " street " + country;
+
+console.log(global_address);
+
+//Ex2
+
+let birthYear = 1992;
+let futureYear = 3000;
+
+let possibleAge = "I will be " + (futureYear - birthYear) + ' in ' + futureYear;
+console.log(possibleAge);
+
+let stuff = ["@","$","%"];
+//length is 3. position is 2.
+
+
+//nested array [[]]
+
+
+// prompt is used to recieve info from the user
+
+
+
+// // 1. Create a numerically indexed table (ie. an array): pets, like this ['cat','dog','fish','rabbit','cow']
+// let pets = ["cat", "dog", "frog","rabbit", "cow"];
+
+// // 2. Display dog
+// console.log(pets[1]);
+
+// // 3. Add to the array pets, the pet horse. Remove the pet rabbit
+// pets.push("horse");
+// pets.splice(3,1,);
+
+// // 4. Display the array length
+
+
+// console.log(pets.length);
+// console.log(pets);
+
