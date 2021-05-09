fdcc6796e907d3889cfc0601a537a0c1a0562b02
YechielNW <yechielallaround@gmail.com>
2021-05-09 22:15:10 +0300

    dy

---
+++ week6/day1/daily Challenge/script.js
@@ -0,0 +1,13 @@
+select  count actor
+from actor_id
+where
+--not sure where to count
+
+-- insert into actor(
+-- 	actor_id,
+-- 	first_name,
+-- 	last_name,
+-- 	last_update
+-- )
+-- values(201,'Yechiel','Widlan','01/06/1992')
+--not sure if i can add without an input
