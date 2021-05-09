2f827aab11a543530429816c51e8fbe6674126d6
YechielNW <yechielallaround@gmail.com>
2021-05-09 21:50:17 +0300

    xp+

---
+++ week6/day1/XP +/script.js
@@ -0,0 +1,41 @@
+
+-- create table students (
+-- 	id serial PRIMARY KEY,
+-- 	last_name varchar(200),
+-- 	first_name varchar(100),
+-- 	birth_date varchar(100)
+-- )
+-- insert into students (
+-- 	last_name,
+-- 	first_name,
+-- 	birth_date
+-- )
+-- values('Benichou','Marc', '02/11/1998'),
+-- ('Cohen','Yoan','03/12/2010'),
+-- ('Benichou','Lea','27/07/1987'),
+-- ('Dux','Amelia','07/04/1996'),
+-- ('Grez','David','14/06/2003'),
+-- ('Simpson','','03/10/1980')
+-- update students
+-- set last_name =
+
+-- insert into students (
+-- 	last_name,
+--  	first_name,
+--  	birth_date
+-- )
+-- values('Widlan','Yechiel','01/06/1992')
+
+ select * from students
+--  where (id=2)
+--  where(last_name = 'Benichou') and (first_name = 'Marc')
+
+-- where(first_name like '%a%') -- contains a
+-- where(first_name like 'A%') --starts with
+-- where(first_name like '%a') --ends with
+-- where(first_name like '%a_') --second to last
+
+-- need answer for this where id='1' and id='3' 
+ -- need to ask abut this where(birth_dates >= '1/01/2000' )
+ 
+ 
